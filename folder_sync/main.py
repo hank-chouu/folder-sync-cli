bc63f145524d169beb0680acc8f529bc42632358
Hank Chou <hengtse.me@gmail.com>
2023-10-30 16:56:11 +0800

    finish command pull, push

--- folder_sync/main.py
+++ folder_sync/main.py
@@ -1,24 +1,29 @@
 import os
 import re
 import subprocess
+import sys
 from configparser import ConfigParser
 
-import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import click
 
-from folder_sync.utils import is_cmd_valid, is_remote_destination_valid, is_local_valid, validation, show_all_remotes, default_name
+from folder_sync.utils import (
+    default_name,
+    get_remotes,
+    is_cmd_valid,
+    is_local_folder_valid,
+    is_remote_folder_valid,
+    show_all_remotes,
+    validation,
+)
 
 pairs = ConfigParser()
 pairs_path = os.path.abspath(os.path.dirname(__file__)) + "/pairs.ini"
 if not os.path.exists(pairs_path):
     with open(pairs_path, "w") as f:
-        pairs.write(f)   
+        pairs.write(f)
 pairs.read(pairs_path)
-
-
-
 
 
 @click.group()
@@ -29,18 +34,20 @@ def cli():
 @click.command()
 def new():
     if not is_cmd_valid("rclone"):
-        click.echo("Error: you need to first install rclone on this machine.")
+        click.secho(
+            "Error: you need to first install rclone on this machine.", fg="red", bold=True
+        )
         raise click.exceptions.Exit(code=1)
     click.echo("\n::: Making a new local/remote pair :::\n")
 
     # local folder
     local = click.prompt(click.style("Local folder path", fg="cyan"))
-    while not is_local_valid(local):
+    while not is_local_folder_valid(local):
         click.secho("Invalid local path. Please try again.\n", fg="red", bold=True)
         local = click.prompt(click.style("Local folder path", fg="cyan"))
 
     # remote stroage
-    all_remotes = subprocess.run(["rclone", "listremotes"], capture_output=True, text=True).stdout.split(" ")
+    all_remotes = get_remotes()
     show_all_remotes(all_remotes)
     remote_idx = click.prompt(click.style("\nEnter the remote # to pair", fg="cyan"))
     while remote_idx not in [str(idx) for idx in range(len(all_remotes))]:
@@ -50,27 +57,55 @@ def new():
     remote = all_remotes[int(remote_idx)].strip("\n")
 
     # remote folder
-    remote_folder = click.prompt(click.style("\nEnter the remote folder, or leave blank to sync the whole remote stroage", fg="cyan"), default="")
-    path_regex = r'^[a-zA-Z0-9_/-]*(/[a-zA-Z0-9_/-]+)*$'
+    remote_folder = click.prompt(
+        click.style(
+            "\nEnter the remote folder, or leave blank to sync the whole remote stroage",
+            fg="cyan",
+        ),
+        default="",
+    )
+    path_regex = r"^[a-zA-Z0-9_/-]*(/[a-zA-Z0-9_/-]+)*$"
     while not re.match(path_regex, remote_folder):
         click.secho(f"Invalid value. Please try again.", fg="red", bold=True)
-        remote_folder = click.prompt(click.style("\nEnter the remote folder, or leave blank to sync the whole remote stroage", fg="cyan"), default="")   
-    while not is_remote_destination_valid(remote, remote_folder):
-        click.secho(f"Invalid path for the remote stroage. Please try again.", fg="red", bold=True)
-        remote_folder = click.prompt(click.style("\nEnter the remote folder, or leave blank to sync the whole remote stroage", fg="cyan"), default="")
-        path_regex = r'^[a-zA-Z0-9_/-]*(/[a-zA-Z0-9_/-]+)*$'
+        remote_folder = click.prompt(
+            click.style(
+                "\nEnter the remote folder, or leave blank to sync the whole remote stroage",
+                fg="cyan",
+            ),
+            default="",
+        )
+    while not is_remote_folder_valid(remote, remote_folder):
+        click.secho(
+            f"Invalid path for the remote stroage. Please try again.", fg="red", bold=True
+        )
+        remote_folder = click.prompt(
+            click.style(
+                "\nEnter the remote folder, or leave blank to sync the whole remote stroage",
+                fg="cyan",
+            ),
+            default="",
+        )
+        path_regex = r"^[a-zA-Z0-9_/-]*(/[a-zA-Z0-9_/-]+)*$"
         while not re.match(path_regex, remote_folder):
             click.secho(f"Invalid value. Please try again.", fg="red", bold=True)
-            remote_folder = click.prompt(click.style("\nEnter the remote folder, or leave blank to sync the whole remote stroage", fg="cyan"), default="")
+            remote_folder = click.prompt(
+                click.style(
+                    "\nEnter the remote folder, or leave blank to sync the whole remote stroage",
+                    fg="cyan",
+                ),
+                default="",
+            )
     click.secho(f"Passed!", fg="green", bold=True)
     if remote_folder and remote_folder[-1] != "/":
-        remote_folder += "/" 
+        remote_folder += "/"
 
     # pair name
     name = click.prompt(click.style("\nName this pair", fg="cyan"), default=default_name(local))
     while name in pairs.sections():
         click.secho(f"Duplicated pair name. Please try again.", fg="red", bold=True)
-        name = click.prompt(click.style("\nName this pair", fg="cyan"), default=default_name(local))
+        name = click.prompt(
+            click.style("\nName this pair", fg="cyan"), default=default_name(local)
+        )
 
     # write to pairs.ini
     pairs[name] = {}
@@ -87,72 +122,79 @@ def new():
     click.echo(" to sync remote from local.")
 
 
+@click.command()
+@click.argument("name", type=str)
+@click.option("--use-copy", is_flag=True, help="Use rclone copy instead of sync.")
+@click.option("--fast", is_flag=True, help="Skip folder validations.")
+def pull(name, use_copy, fast):
+    """Pull from remote folder."""
 
+    if not is_cmd_valid("rclone"):
+        click.secho(
+            "Error: you need to first install rclone on this machine.", fg="red", bold=True
+        )
+        raise click.exceptions.Exit(code=1)
 
+    if name not in pairs.sections():
+        click.secho(f"Invalid pair name. Please try again.", fg="red", bold=True)
+        raise click.exceptions.Exit(code=1)
 
+    program = "rclone"
+    cmd = "sync"
+    if use_copy:
+        cmd = "copy"
+    local_folder = pairs.get(name, "local")
+    remote_full = pairs.get(name, "remote")
+    if not fast:
+        validation(local_folder, remote_full)
+    try:
+        click.echo("Pull started.")
+        result = subprocess.run(
+            [program, cmd, remote_full, local_folder, "-P"], stderr=subprocess.PIPE, text=True
+        )
+        if not result.stderr:
+            click.echo("Pull completed.")
+        else:
+            click.echo(result.stderr.strip("\n"))
+    except subprocess.CalledProcessError as e:
+        click.echo(e)
 
 
 @click.command()
 @click.argument("name", type=str)
 @click.option("--use-copy", is_flag=True, help="Use rclone copy instead of sync.")
-@click.option("--fast", is_flag=True, help="Skip path validations.")
-def pull(use_copy, fast):
-    """Pull from remote folder."""
-    # if not is_rclone_exists():
-    #     click.echo("Error: no rclone installed on this machine.")
-    #     raise click.exceptions.Exit(code=1)
-
-    # program = "rclone"
-    # cmd = "sync"
-    # if use_copy:
-    #     cmd = "copy"
-    # local = config_object.get("DIR_PATH", "local")
-    # remote = config_object.get("DIR_PATH", "remote")
-    # if not fast:
-    #     click.echo("Validating config files...")
-    #     validation(local, remote)
-    # try:
-    #     click.echo("Pull started.")
-    #     result = subprocess.run(
-    #         [program, cmd, remote, local, "-P"], stderr=subprocess.PIPE, text=True
-    #     )
-    #     if not result.stderr:
-    #         click.echo("Pull completed.")
-    #     else:
-    #         click.echo(result.stderr.strip("\n"))
-    # except subprocess.CalledProcessError as e:
-    #     click.echo(e)
-
-
-@click.command()
-@click.option("--use-copy", is_flag=True, help="Use rclone copy instead of sync.")
-@click.option("--fast", is_flag=True, help="Skip path validations.")
-def push(use_copy, fast):
+@click.option("--fast", is_flag=True, help="Skip folder validations.")
+def push(name, use_copy, fast):
     """Push local to remote."""
-    # if not is_rclone_exists():
-    #     click.echo("Error: no rclone installed on this machine.")
-    #     raise click.exceptions.Exit(code=1)
+    if not is_cmd_valid("rclone"):
+        click.secho(
+            "Error: you need to first install rclone on this machine.", fg="red", bold=True
+        )
+        raise click.exceptions.Exit(code=1)
 
-    # program = "rclone"
-    # cmd = "sync"
-    # if use_copy:
-    #     cmd = "copy"
-    # dirs = config_object["DIR_PATH"]
-    # local = dirs.get("local")
-    # remote = dirs.get("remote")
-    # if not fast:
-    #     click.echo("Validating config files...")
-    #     validation(local, remote)
-    # try:
-    #     result = subprocess.run(
-    #         [program, cmd, local, remote, "-P"], stderr=subprocess.PIPE, text=True
-    #     )
-    #     if not result.stderr:
-    #         click.echo("Push completed.")
-    #     else:
-    #         click.echo(result.stderr.strip("\n"))
-    # except subprocess.CalledProcessError as e:
-    #     click.echo(e)
+    if name not in pairs.sections():
+        click.secho(f"Invalid pair name. Please try again.", fg="red", bold=True)
+        raise click.exceptions.Exit(code=1)
+
+    program = "rclone"
+    cmd = "sync"
+    if use_copy:
+        cmd = "copy"
+    local_folder = pairs.get(name, "local")
+    remote_full = pairs.get(name, "remote")
+    if not fast:
+        validation(local_folder, remote_full)
+    try:
+        click.echo("Push Started.")
+        result = subprocess.run(
+            [program, cmd, local_folder, remote_full, "-P"], stderr=subprocess.PIPE, text=True
+        )
+        if not result.stderr:
+            click.echo("Push completed.")
+        else:
+            click.echo(result.stderr.strip("\n"))
+    except subprocess.CalledProcessError as e:
+        click.echo(e)
 
 
 cli.add_command(new)
--- folder_sync/utils.py
+++ folder_sync/utils.py
@@ -1,17 +1,25 @@
-import subprocess
-import re
 import os
+import subprocess
 
 import click
 
-def show_all_remotes(all_remotes):
 
-    click.secho(f"\nFound {len(all_remotes)} remote destinations from the rclone config:", fg="cyan")
+def get_remotes() -> list:
+    return subprocess.run(
+        ["rclone", "listremotes"], capture_output=True, text=True
+    ).stdout.strip("\n").split("\n")
+
+
+def show_all_remotes(all_remotes):
+    click.secho(
+        f"\nFound {len(all_remotes)} remote destinations from the rclone config:", fg="cyan"
+    )
     for i in range(len(all_remotes)):
         click.echo("[ ", nl=False)
         click.secho(i, fg="green", nl=False)
-        remote_strip = all_remotes[i].strip("\n")
+        remote_strip = all_remotes[i]
         click.echo(f"]: {remote_strip}")
+
 
 def is_cmd_valid(cmd):
     try:
@@ -24,22 +32,31 @@ def is_cmd_valid(cmd):
         return True
     except subprocess.CalledProcessError:
         return False
-    
-def is_local_valid(local_dir):
-    return os.path.exists(local_dir) and os.path.isdir(local_dir)
 
 
-def is_remote_destination_valid(remote, folder:str):
+def is_local_folder_valid(local_folder):
+    return os.path.exists(local_folder) and os.path.isdir(local_folder)
 
+
+def is_remote_valid(remote):
+    remotes = get_remotes()
+    if remote in remotes:
+        return True
+    return False
+
+
+def is_remote_folder_valid(remote, folder: str):
     # usnig whole remote stroage
     if not folder:
         return True
-    click.secho(f"\nValidating remote folder...", fg="cyan")
+    click.secho(f"Validating remote folder...", fg="cyan")
     depth = len(folder.strip("/").split("/"))
     result = subprocess.run(
-        ["rclone", "lsf", "--dirs-only", remote, f"--max-depth={depth}"], capture_output=True, text=True
+        ["rclone", "lsf", "--dirs-only", remote, f"--max-depth={depth}"],
+        capture_output=True,
+        text=True,
     )
-    folders = result.stdout.strip("n").split("\n")    
+    folders = result.stdout.strip("n").split("\n")
     if folder[-1] != "/":
         folder += "/"
     if folder not in folders:
@@ -51,18 +68,28 @@ def default_name(local_folder):
     return local_folder.strip("/").split("/")[-1].lower()
 
 
+def validation(local_folder, remote_full):
 
+    click.secho("\nValidating pair configuration...", fg="cyan")
+    if not local_folder:
+        click.secho("Error: local is not configured.", fg="red", bold=True)
+        raise click.exceptions.Exit(code=1)
+    if not remote_full:
+        click.secho("Error: reomte is not configured.", fg="red", bold=True)
+        raise click.exceptions.Exit(code=1)
+    if not is_local_folder_valid(local_folder):
+        click.secho(f"Error: local folder {local_folder} is not valid.", fg="red", bold=True)
+        raise click.exceptions.Exit(code=1)
 
-def validation(local, remote):
-    if not local:
-        click.echo("Error: local path is not configured.")
-        raise click.exceptions.Exit(code=1)
-    if not remote:
-        click.echo("Error: reomte path is not configured.")
-        raise click.exceptions.Exit(code=1)
-    if not is_local_valid(local):
-        click.echo("Error: local path is not valid.")
-        raise click.exceptions.Exit(code=1)
+    remote = remote_full.split(":")[0] + ":"
+    remote_folder = remote_full.split(":")[1]
+
     if not is_remote_valid(remote):
-        click.echo("Error: reomte path is not valid.")
+        click.secho(
+            f"Error: remote stroage {remote} is not valid.", fg="red", bold=True
+        )
+        raise click.exceptions.Exit(code=1)
+
+    if not is_remote_folder_valid(remote, remote_folder):
+        click.secho(f"Error: remote folder {remote_folder} is not valid.", fg="red", bold=True)
         raise click.exceptions.Exit(code=1)
