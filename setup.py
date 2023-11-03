2e31dbba6839fc0b828c9cc569814298ce2c8dc1
Hank Chou <hengtse.me@gmail.com>
2023-11-03 14:26:14 +0800

    add ver. number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="folder-sync-cli",
-    version="1.1.5",
+    version="1.1.6",
     packages=find_packages(),
     install_requires=["click"],
     description="A CLI tool to simplify rclone sync.",
